fefd96429382c9e0b9f89fde4f950dfa2be5926c
Ossama Ismaili <oussama@chari.ma>
2022-01-15 19:29:54 +0100

    Update project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tuts'),
+      dir: require('path').join(__dirname, './coverage/Task Tracker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tuts'`, () => {
+  it(`should have as title 'Task Tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tuts');
+    expect(app.title).toEqual('Task Tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tuts app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Task Tracker app is running!');
   });
 });
